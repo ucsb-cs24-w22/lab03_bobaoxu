b2cb38b1acec0d8d29f1000d02754e424e412af2
AO XU <ao_xu@cs.ucsb.edu>
2022-02-02 14:14:28 -0800

    AO

--- intlist.cpp
+++ intlist.cpp
@@ -70,7 +70,7 @@ int IntList::max() const {
 // 0 if list is empty
 double IntList::average() const {
 	if (count()==0) return 0.0;
-	return (sum()/count());  // REPLACE THIS NON-SOLUTION
+	return sum()/(double)count();  // REPLACE THIS NON-SOLUTION
 }
 
 // inserts value as new node at beginning of list
